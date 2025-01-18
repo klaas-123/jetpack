e811a876ae0056c632b65e94f5d4f00bffdac1d6
Klaas Adriaenssen <klaas.adriaenssen@gmail.com>
2025-01-18 22:59:09 +0100

    begin

---
+++ src/client/java/org/snekker/jetpack/client/JetpackClient.java
@@ -0,0 +1,10 @@
+package org.snekker.jetpack.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JetpackClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/snekker/jetpack/Jetpack.java
@@ -0,0 +1,10 @@
+package org.snekker.jetpack;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Jetpack implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
