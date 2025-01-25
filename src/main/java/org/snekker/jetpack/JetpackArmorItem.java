87ac5237b3fc12e508a53614cd4670156038b03c
Klaas Adriaenssen <klaas.adriaenssen@gmail.com>
2025-01-25 22:10:51 +0100

    Jetpack Itembar

--- src/main/java/org/snekker/jetpack/JetpackArmorItem.java
+++ src/main/java/org/snekker/jetpack/JetpackArmorItem.java
@@ -2,12 +2,19 @@ package org.snekker.jetpack;
 
 
 import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.equipment.ArmorMaterials;
 import net.minecraft.item.equipment.EquipmentType;
+import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.text.Text;
+import org.snekker.jetpack.component.ModComponents;
+
+import java.util.List;
+import java.util.Stack;
 
 public class JetpackArmorItem extends ArmorItem {
     public JetpackArmorItem(Settings settings) {
-        super(ArmorMaterials.ARMADILLO_SCUTE, EquipmentType.CHESTPLATE, settings);
+        super(ArmorMaterials.ARMADILLO_SCUTE, EquipmentType.CHESTPLATE, settings.component(ModComponents.JETPACK_FUEL_COMPONENT, 2000));
 
     /*
     @Override
@@ -29,5 +36,27 @@ public class JetpackArmorItem extends ArmorItem {
 
 
         return ActionResult.SUCCESS;*/
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+        super.appendTooltip(stack, context, tooltip, type);
+        tooltip.add(Text.literal(stack.getOrDefault(ModComponents.JETPACK_FUEL_COMPONENT, 0) + " " + "Fuel"));
+    }
+
+    @Override
+    public int getItemBarStep(ItemStack stack) {
+        return (int) Math.ceil(stack.getOrDefault(ModComponents.JETPACK_FUEL_COMPONENT, 0) * 13.0F / 2000.0F);
+
+    }
+
+    @Override
+    public boolean isItemBarVisible(ItemStack stack) {
+        return true;
+    }
+
+    @Override
+    public int getItemBarColor(ItemStack stack) {
+        return 0xff0001;
     }
 }
