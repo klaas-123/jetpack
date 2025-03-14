49ac95f0cff2fe7ef108186a8cd216b8a0233064
Klaas Adriaenssen <klaas.adriaenssen@gmail.com>
2025-03-14 10:04:29 +0100

    screen of the recharge station works, no moving textures yet

--- src/main/java/org/snekker/jetpack/block/RechargeStationEntity.java
+++ src/main/java/org/snekker/jetpack/block/RechargeStationEntity.java
@@ -29,6 +29,8 @@ import org.snekker.jetpack.screens.RechargeStationScreenHandler;
 public class RechargeStationEntity extends BlockEntity implements NamedScreenHandlerFactory, InventoryChangedListener {
     public final SimpleInventory inventory;
     public final PropertyDelegate propertyDelegate;
+    private int fuelLeft = 0;
+    private int fuelMax = 0;
 
 
     public RechargeStationEntity(BlockPos pos, BlockState state) {
@@ -61,32 +63,32 @@ public class RechargeStationEntity extends BlockEntity implements NamedScreenHan
         return inventory.getStack(0);
     }
 
-    public ItemStack setIngredientSlotStack() {
-        return inventory.getStack(0);
+    public void setIngredientSlotStack(ItemStack stack) {
+        inventory.setStack(0, stack);
     }
 
     public ItemStack getEmptyCellSlotStack() {
-        return inventory.getStack(0);
+        return inventory.getStack(1);
     }
 
-    public ItemStack setEmptyCellSlotStack() {
-        return inventory.getStack(0);
+    public void setEmptyCellSlotStack(ItemStack stack) {
+        inventory.setStack(1, stack);
     }
 
     public ItemStack getCellSlotStack() {
-        return inventory.getStack(0);
+        return inventory.getStack(2);
     }
 
-    public ItemStack setCellSlotStack() {
-        return inventory.getStack(0);
+    public void setCellSlotStack(ItemStack stack) {
+        inventory.setStack(2, stack);
     }
 
     public ItemStack getFuelSlotStack() {
-        return inventory.getStack(0);
+        return inventory.getStack(3);
     }
 
-    public ItemStack setFuelSlotStack() {
-        return inventory.getStack(0);
+    public void setFuelSlotStack(ItemStack stack) {
+        inventory.setStack(3, stack);
     }
 
     @Override
@@ -98,4 +100,70 @@ public class RechargeStationEntity extends BlockEntity implements NamedScreenHan
     public @Nullable ScreenHandler createMenu(int syncId, PlayerInventory playerInventory, PlayerEntity player) {
         return new RechargeStationScreenHandler(syncId, playerInventory, inventory, propertyDelegate);
     }
+
+    public void tick(World world, BlockPos pos, BlockState state) {
+        var fuelStack = getFuelSlotStack();
+        var ingredientStack = getIngredientSlotStack();
+        var cellStack = getCellSlotStack();
+        var emptyCellStack = getEmptyCellSlotStack();
+        var hasFuel = !fuelStack.isEmpty();
+        var hasIngredient = !ingredientStack.isEmpty();
+        var hasCell = !cellStack.isEmpty();
+        var hasEmptyCell = !emptyCellStack.isEmpty();
+
+
+        if (hasEmptyCell && hasIngredient) {
+            if (!hasFuel) {
+                fuelLeft = fuelMax = 0;
+                return;
+            }
+
+            if (fuelLeft == 0) {
+                if (hasFuel) {
+                    var replaceWithBucket = fuelStack.getItem() instanceof BucketItem;
+                    fuelLeft = fuelMax = world.getFuelRegistry().getFuelTicks(fuelStack);
+                    if (fuelLeft > 0) {
+                        fuelStack.decrement(1);
+                        if (replaceWithBucket) {
+                            setFuelSlotStack(new ItemStack(Items.BUCKET));
+                        }
+                    }
+                } else {
+                    fuelMax = 0;
+                }
+            }
+
+            if (fuelLeft > 0) {
+                fuelLeft = Math.clamp(fuelLeft - 16 , 0, fuelLeft);
+                if (fuelLeft == 0) {
+                    if (!hasCell) {
+                        setCellSlotStack(new ItemStack(ModItems.FUEL_CELL));
+                    } else if (hasCell) {
+                        cellStack.increment(1);
+                    }
+                    emptyCellStack.decrement(1);
+                    ingredientStack.decrement(1);
+                }
+            }
+
+
+            /*
+
+            emptyCellStack.decrement(1);
+            ingredientStack.decrement(1);
+            fuelStack.decrement(1);
+            if (!hasCell) {
+                setCellSlotStack(new ItemStack(ModItems.FUEL_CELL));
+            } else if (hasCell) {
+                cellStack.increment(1);
+            }
+
+             */
+
+        }
+
+    }
+
+    @Override
+    public void onInventoryChanged(Inventory sender) {}
 }
